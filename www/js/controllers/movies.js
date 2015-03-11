9f9bd50cf09cbf0ec2872bb1baf416dffda78516
Tommy Jiang <tianyangj@gmail.com>
2015-03-10 21:58:32 -0700

    add movies

--- www/js/controllers/movies.js
+++ www/js/controllers/movies.js
@@ -2,6 +2,56 @@
 
 angular.module('GameFly')
 
-.controller('moviesController', function($scope) {
-  console.log('moviesController', $scope);
+.controller('moviesController', function($scope, $stateParams, merchandisingService, productService, platformConfig) {
+  
+  $scope.platform = platformConfig.get('movies', $stateParams.platform);
+
+  $scope.platformIds = $scope.platform ? [$scope.platform.id] : [];
+
+  merchandisingService.get('movies', $scope.platformIds).then(function(merchandising) {
+    $scope.merchandising = merchandising;
+  });
+
+  productService.query({
+  	mostpopular: true,
+    platforms: $scope.platformIds,
+  	productType: 'Movie',
+  	'sort.direction': 'desc',
+  	'sort.field': 'mostpopular'
+  }).then(function(data) {
+  	$scope.mostpopular = data;
+  });
+
+  productService.query({
+  	newreleases: true,
+    platforms: $scope.platformIds,
+  	productType: 'Movie',
+  	'sort.direction': 'desc',
+  	'sort.field': 'releasedate'
+  }).then(function(data) {
+  	$scope.newreleases = data;
+  });
+
+  productService.query({
+  	comingsoon: true,
+    platforms: $scope.platformIds,
+  	productType: 'Movie',
+  	'sort.direction': 'asc',
+  	'sort.field': 'releasedate'
+  }).then(function(data) {
+  	$scope.comingsoon = data;
+  });
+
+  productService.query({
+  	bestselling: true,
+  	newOnly: false,
+  	usedOnly: true,
+    platforms: $scope.platformIds,
+  	productType: 'Movie',
+  	'sort.direction': 'desc',
+  	'sort.field': 'usedbestselling'
+  }).then(function(data) {
+  	$scope.usedbestsellers = data;
+  });
+
 });
