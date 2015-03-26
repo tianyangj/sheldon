8cc83203886ba1e83f4ca0affa914ae65b6bf5a3
Tommy Jiang <tianyangj@gmail.com>
2015-03-25 23:46:30 -0700

    improve search page

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -2,7 +2,42 @@
 
 angular.module('GameFly')
 
-.controller('searchController', function($scope) {
+.controller('searchController', function($scope, $timeout, productService) {
 
-  console.log('searchController')
+  var timeout;
+
+  $scope.data = {};
+
+  $scope.onFocus = function() {
+    console.log('focused...')
+    $scope.isFocused = true;
+  };
+
+  $scope.onBlur = function() {
+    console.log('blured...')
+    $scope.isFocused = false;
+  };
+
+  $scope.search = function() {
+    console.log('searching...', $scope.data)
+    if ($scope.data.query === '') {
+      $timeout.cancel(timeout);
+      $scope.items = [];
+      return;
+    }
+    if (timeout) {
+      $timeout.cancel(timeout);
+    }
+    timeout = $timeout(function() {
+      timeout = null;
+      productService.query({
+        'search.keywords': $scope.data.query,
+        'search.titleonly': true,
+        'sort.direction': 'desc',
+        'sort.field': 'mostpopular'
+      }).then(function(data) {
+        $scope.items = data.items;
+      });
+    }, 300);
+  };
 });
