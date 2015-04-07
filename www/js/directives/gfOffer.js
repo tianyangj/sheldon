8d3aba1e73d85703bdd3e5d0252ca56afe19d3b5
Tommy Jiang <tianyangj@gmail.com>
2015-04-07 00:23:15 -0700

    new offer actions

--- www/js/directives/gfOffer.js
+++ www/js/directives/gfOffer.js
@@ -6,25 +6,61 @@ angular.module('GameFly')
 
 	return {
 		restrict: 'E',
-		templateUrl: 'templates/partials/offer.html',
+		templateUrl: 'templates/partials/offer1.html',
 		scope: {
 			offers: '='
 		},
-		controller: function($scope) {
+		controller: function($scope, $state) {
+			$scope.onRent = function() {
+				if ($scope.rentOffer.isAlreadyInQueue) {
+					$state.go('app.queue');
+				} else if ($scope.rentOffer.isRented) {
+					console.log('todo: move to cart', $scope.keepOffer);
+				} else {
+					console.log('todo: call parent scope.rent', $scope.rentOffer);
+				}
+			};
+			$scope.onPurchase = function() {
+				console.log('todo: ', $scope.purchaseAction);
+			};
 			$scope.$watch('offers', function(newVal, oldVal) {
 				if (newVal !== oldVal) {
-					$scope.rentOffer = _(newVal).find(function(offer) {
+					var rentOffer = _(newVal).find(function(offer) {
 						return offer.offerActionType === 'Rent';
 					});
-					$scope.newOffer = _(newVal).find(function(offer) {
+					var newOffer = _(newVal).find(function(offer) {
 						return offer.offerActionType === 'Purchase' && !offer.skuIsUsed;
 					});
-					$scope.usedOffer = _(newVal).find(function(offer) {
+					var usedOffer = _(newVal).find(function(offer) {
 						return offer.offerActionType === 'Purchase' && offer.skuIsUsed;
 					});
-					$scope.keepOffer = _(newVal).find(function(offer) {
+					var keepOffer = _(newVal).find(function(offer) {
 						return offer.offerActionType === 'Keep';
 					});
+					// normalize rentOffer and keepOffer
+					if (rentOffer && rentOffer.canRent) {
+						if (keepOffer && rentOffer.isRented) {
+							$scope.keepOffer = keepOffer;
+						}
+						$scope.rentOffer = rentOffer;
+					}
+					// normalize newOffer and usedOffer
+					if (newOffer && usedOffer) {
+						$scope.purchaseAction = 'Buy';
+						$scope.newOffer = newOffer;
+						$scope.usedOffer = usedOffer;
+					} else if (newOffer && !usedOffer) {
+						if (newOffer.isPreorderOffer) {
+							$scope.purchaseAction = 'Pre-Order';
+							$scope.newOffer = newOffer;
+						} else {
+							$scope.purchaseAction = 'Buy New';
+							$scope.newOffer = newOffer;
+						}
+					} else if (usedOffer && !newOffer) {
+						$scope.purchaseAction = 'Buy Used';
+						$scope.usedOffer = usedOffer;
+					}
 				}
 			});
 		}
