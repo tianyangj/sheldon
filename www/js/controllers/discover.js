ca105dd437569e9bbccbaa335f6a5e2e59d70cd0
Tommy Jiang <tianyangj@gmail.com>
2015-04-29 00:21:15 -0700

    discover page ui improvement

--- www/js/controllers/discover.js
+++ www/js/controllers/discover.js
@@ -11,5 +11,27 @@ angular.module('GameFly')
 		'sort.field': 'mostpopular'
 	}).then(function(data) {
 		$scope.suggestions = data.items;
+		$scope.cards = angular.copy(data.items.slice(0, 3));
 	});
+
+	$scope.cardSwiped = function(index) {
+		console.log('cardSwiped', index);
+	};
+
+	$scope.cardSwipedLeft = function(index) {
+        console.log('Left swipe', index);
+    }
+ 
+    $scope.cardSwipedRight = function(index) {
+        console.log('Right swipe', index);
+    }
+
+	$scope.cardDestroyed = function(index) {
+		console.log('cardDestroyed', index);
+		
+		$scope.cards.splice(index, 1);
+
+		var newCard = $scope.suggestions[Math.floor(Math.random() * $scope.suggestions.length)];
+    	$scope.cards.splice(0, 0, angular.extend({}, newCard));
+	};
 });
